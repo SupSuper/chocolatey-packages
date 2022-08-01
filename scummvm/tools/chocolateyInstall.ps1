f0cc3eb0d55588a6b6cf8b9525edaeb4ea0baefc
SupSuper <supsuper@gmail.com>
2022-08-01 20:31:19 +0100

    Update ScummVM to 2.6.0

--- scummvm/tools/chocolateyInstall.ps1
+++ scummvm/tools/chocolateyInstall.ps1
@@ -5,11 +5,11 @@ $packageArgs = @{
   fileType      = 'exe'
   softwareName  = 'ScummVM*'
 
-  url           = 'https://downloads.scummvm.org/frs/scummvm/2.5.1/scummvm-2.5.1-win32.exe'
+  url           = 'https://downloads.scummvm.org/frs/scummvm/2.6.0/scummvm-2.6.0-win32.exe'
   silentArgs    = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-' # Inno Setup
   validExitCodes= @(0)
 
-  checksum      = 'e5e15c06d5d5c2f96c7c900e9e4a512390ca2a569e2f3a13667cdb09c87c3ce0'
+  checksum      = 'a01b3f92c8e03041596c3f8e3383a0d4a3ddec5af6407bdc9199f4dd1c497cb8'
   checksumType  = 'sha256'
 }
 
