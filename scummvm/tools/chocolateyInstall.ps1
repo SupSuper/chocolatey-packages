6ffdc39d48c3869a6eef88f24d41d2b0f177b2bc
SupSuper <supsuper@gmail.com>
2024-12-23 00:38:01 +0000

    Update ScummVM to 2.9.0

--- scummvm/tools/chocolateyInstall.ps1
+++ scummvm/tools/chocolateyInstall.ps1
@@ -5,11 +5,11 @@ $packageArgs = @{
   fileType      = 'exe'
   softwareName  = 'ScummVM*'
 
-  url           = 'https://downloads.scummvm.org/frs/scummvm/2.8.1/scummvm-2.8.1-win32.exe'
+  url           = 'https://downloads.scummvm.org/frs/scummvm/2.9.0/scummvm-2.9.0-win32.exe'
   silentArgs    = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-' # Inno Setup
   validExitCodes= @(0)
 
-  checksum      = '4fb0d9426465aa6b245871fbb268c903b9bf4aee91b01f3a54363369a682348e'
+  checksum      = '0f50ac5524f691b7c0f65a6ab9169cc7f1fe07e7dbe17a296bd3cfe3bfeccd12'
   checksumType  = 'sha256'
 }
 
